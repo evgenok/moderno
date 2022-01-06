33067e165150b2dabec68bb4e89c3a324a32bc86
evgenok <popovevgen18042001@mail.ru>
2022-01-06 19:08:27 +0300

    firsts day

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src('app/scss/**/*.scss')
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
